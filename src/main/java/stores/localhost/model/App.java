591c90f761384f664f9d094180f52b53249dcfb0
surb-hi <surbhikakadiya1005@gmail.com>
2022-06-07 14:18:01 +0100

    project5_swagger

---
+++ src/main/java/stores/localhost/model/App.java
@@ -0,0 +1,13 @@
+package stores.localhost.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
